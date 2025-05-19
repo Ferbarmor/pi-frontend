e80212770d567c4f05e5b8cf6f60cc027b2d20f9
Ferbarmor <ferbarmor@gmail.com>
2025-05-19 21:23:55 +0200

    Carrusel nuevo

--- src/app/components/carrusel/carrusel.component.ts
+++ src/app/components/carrusel/carrusel.component.ts
@@ -1,38 +1,104 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-carrusel',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './carrusel.component.html',
   styleUrl: './carrusel.component.css'
 })
 export class CarruselComponent {
-  constructor() {
+  currentIndex = 0;
+  intervalId: any;
+  isMobile = false;
+  slides = [
+    {
+      title: 'SnapTapea en primavera',
+      class: 'ninoescalada',
+      content: '',
+      specialText: true
+    },
+    {
+      title: 'Siente la pasión de la imagen',
+      class: 'bici',
+      content: ''
+    },
+    {
+      title: 'La primavera, la sangre...',
+      class: 'buceo',
+      content: ''
+    },
+    {
+      title: 'Capta la esencia',
+      class: 'crossfit',
+      content: ''
+    },
+  ];
+
+  ngOnInit() {
+    this.checkScreenSize();
     this.startAutoSlide();
   }
 
+  ngOnDestroy() {
+    this.stopAutoSlide();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.isMobile = window.innerWidth < 768;
+  }
+
+  startAutoSlide() {
+    this.stopAutoSlide();
+    this.intervalId = setInterval(() => {
+      this.nextSlide();
+    }, 10000);
+  }
+
+  stopAutoSlide() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
   nextSlide() {
-    const items = document.querySelectorAll('.item');
-    const slideContainer = document.querySelector('.slide');
-    if (slideContainer) {
-      slideContainer.appendChild(items[0] as Node);
+    this.currentIndex = (this.currentIndex + 1) % this.slides.length;
+    if (this.isMobile) {
+      this.scrollToCurrentSlide();
     }
   }
 
   prevSlide() {
-    const items = document.querySelectorAll('.item');
-    const slideContainer = document.querySelector('.slide');
-    if (slideContainer && items.length > 0) {
-      slideContainer.prepend(items[items.length - 1] as Node);
+    this.currentIndex = (this.currentIndex - 1 + this.slides.length) % this.slides.length;
+    if (this.isMobile) {
+      this.scrollToCurrentSlide();
     }
   }
 
-  startAutoSlide() {
-    setInterval(() => this.nextSlide(), 10000);
+  goToSlide(index: number) {
+    this.currentIndex = index;
+    if (this.isMobile) {
+      this.scrollToCurrentSlide();
+    }
+  }
+
+  private scrollToCurrentSlide() {
+    const slideElement = document.querySelector(`.slide-container .item:nth-child(${this.currentIndex + 1})`);
+    if (slideElement) {
+      slideElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'center'
+      });
+    }
   }
 
 }
-// Inicializa el carrusel
-const carousel = new CarruselComponent();
+
 
 
